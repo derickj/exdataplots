5476354610d2f9319ca457459581a2256210048b
Derick J <derick63@gmail.com>
2015-08-07 10:32:19 +0200

    Added comments to R scripts

--- plot1.R
+++ plot1.R
@@ -1,12 +1,20 @@
+##
+## Test whether the input file exists in the current working directory.
+## If not, display error message and terminate
+##
 inpfile<-"household_power_consumption.txt"
     if(!file.exists(inpfile)) {
         msg<-paste(inpfile, "does not exist", sep=" ")
         stop(msg)
     }
+##
+## Read the data from the file and select the relevant days' data
+##
 thedata<-read.table(inpfile, sep=";", header=TRUE, na.strings="?",stringsAsFactors=FALSE)
 reldata<-thedata[thedata$Date=="1/2/2007"|thedata$Date=="2/2/2007",]
-# thedata$Date<-as.Date(thedata$Date,"%d/%m/%Y")
-# reldata<-thedata[thedata$Date==as.Date("2007-02-01")|thedata$Date==as.Date("2007-02-02"),]
+##
+## Create the plot in a png file in the current working directory
+##
 png(filename="plot1.png")
 hist(reldata$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.off()
--- plot2.R
+++ plot2.R
@@ -1,12 +1,26 @@
+##
+## Test whether the input file exists in the current working directory.
+## If not, display error message and terminate
+##
 inpfile<-"household_power_consumption.txt"
     if(!file.exists(inpfile)) {
         msg<-paste(inpfile, "does not exist", sep=" ")
         stop(msg)
     }
+##
+## Read the data from the file and select the relevant days' data
+##
 thedata<-read.table(inpfile, sep=";", header=TRUE, na.strings="?",stringsAsFactors=FALSE)
 reldata<-thedata[thedata$Date=="1/2/2007"|thedata$Date=="2/2/2007",]
+##
+## Convert the date and time string fields to a POSIX time field, add values as a column called
+## "datetime" to the table
+##
 dates<-paste(reldata$Date,reldata$Time,sep=" ")
 reldata$datetime<-strptime(dates,"%d/%m/%Y %H:%M:%S")
+##
+## Create the plot in a png file in the current working directory
+##
 png(filename="plot2.png")
 plot(reldata$datetime,reldata$Global_active_power,type="l",xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
